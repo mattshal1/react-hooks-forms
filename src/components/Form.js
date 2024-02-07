515830eb1076b72c97b3edab28302710a72c6bad
Ahmed Shalash <ah.shalash@gmail.com>
2024-02-07 07:26:04 +0300

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,13 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  const handleFirstName = (event) => {setFirstName(event.target.value)};
+  const handleLastName = (event) => {setLastName(event.target.value)};
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={handleFirstName}/>
+      <input type="text" value={lastName} onChange={handleLastName}/>
       <button type="submit">Submit</button>
     </form>
   );
